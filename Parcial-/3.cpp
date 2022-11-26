6099f324dc337cc24b948031b8e8adc2e37b6e38
dimunozv <dimunozv@unal.edu.co>
2022-11-26 18:03:41 -0500

    codigos y graficas del parcial

---
+++ Parcial-/3.cpp
@@ -0,0 +1,199 @@
+#include <iostream>
+#include <fstream>
+#include <cmath>
+using namespace std;
+
+const int Lx=256;
+const int Ly=64;
+
+const int Q=9;
+const double mu=Lx/8;
+const double sigma=Ly/9;
+const double deltap=0.001;
+const double eta=10;
+
+const double tau=1.55;
+const double Utau=1.0/tau;
+const double UmUtau=1-Utau;
+
+//--------------------- Clase LatticeBoltzmann ------------
+class LatticeBoltzmann{
+private:
+  double w[Q];      //Weights 
+  int Vx[Q],Vy[Q];  //Velocity vectors
+  double *f, *fnew; //Distribution Functions
+public:
+  LatticeBoltzmann(void);
+  ~LatticeBoltzmann(void);
+  int n(int ix,int iy,int i){return (ix*Ly+iy)*Q+i;};
+  double rho(int ix,int iy,bool UseNew);
+  double Jx(int ix,int iy,bool UseNew);
+  double Jy(int ix,int iy,bool UseNew);
+  double feq(double rho0,double Ux0,double Uy0,int i);
+  void Start(double rho0,double Ux0,double Uy0,double sigma, double mu);
+  void Collision(void);
+  void Advection(void);
+  void Print(const char * NameFile,double Ufan);
+  double Varianza(void);
+};
+LatticeBoltzmann::LatticeBoltzmann(void){
+   //Set the weights
+  w[0]=4.0/9;  w[1]=w[2]=w[3]=w[4]=1.0/9;  w[5]=w[6]=w[7]=w[8]=1.0/36;
+  //Set the velocity vectors
+  Vx[0]=0;  Vx[1]=1;  Vx[2]=0;  Vx[3]=-1; Vx[4]=0;
+  Vy[0]=0;  Vy[1]=0;  Vy[2]=1;  Vy[3]=0;  Vy[4]=-1;
+
+            Vx[5]=1;  Vx[6]=-1; Vx[7]=-1; Vx[8]=1;
+            Vy[5]=1;  Vy[6]=1;  Vy[7]=-1; Vy[8]=-1;
+ //Create the dynamic arrays
+  int ArraySize=Lx*Ly*Q;
+  f=new double [ArraySize];  fnew=new double [ArraySize];
+}
+LatticeBoltzmann::~LatticeBoltzmann(void){
+    delete[] f;  delete[] fnew;
+}
+double LatticeBoltzmann::rho(int ix,int iy,bool UseNew){
+  double sum; int i,n0;
+  for(sum=0,i=0;i<Q;i++){
+    n0=n(ix,iy,i);
+    if(UseNew) sum+=fnew[n0]; else sum+=f[n0];
+  }
+  return sum;
+}  
+double LatticeBoltzmann::Jx(int ix,int iy,bool UseNew){
+  double sum; int i,n0;
+  if(UseNew){
+    sum=0.5*rho(ix,iy,true)*iy*(Ly-1-iy)*deltap/eta;}
+  else {sum=0.5*rho(ix,iy,true)*iy*(Ly-1-iy)*deltap/eta;}
+  return sum;
+}  
+double LatticeBoltzmann::Jy(int ix,int iy,bool UseNew){
+  double sum; int i,n0;
+  for(sum=0,i=0;i<Q;i++){
+    n0=n(ix,iy,i);
+    if(UseNew) sum+=Vy[i]*fnew[n0]; else sum+=Vy[i]*f[n0];
+  }
+  return 0;
+}  
+double  LatticeBoltzmann::feq(double rho0,double Ux0,double Uy0,int i){
+  double UdotVi=Ux0*Vx[i]+Uy0*Vy[i], U2=Ux0*Ux0+Uy0*Uy0;
+  return rho0*w[i]*(1+3*UdotVi+4.5*UdotVi*UdotVi-1.5*U2);
+}  
+void LatticeBoltzmann::Start(double rho0,double Ux0,double Uy0,double sigma, double mu){
+  int ix,iy,i,n0;
+  Ux0=0;
+  Uy0=0;
+  for(ix=0;ix<Lx;ix++){ //for each cell
+    for(iy=0;iy<Ly;iy++){
+    rho0=exp(-0.5*pow((ix-mu)/sigma,2))/(sigma*sqrt(2*M_PI));
+      for(i=0;i<Q;i++){ //on each direction
+	n0=n(ix,iy,i);
+	f[n0]=feq(rho0,Ux0,Uy0,i);
+	
+      }
+    }
+  }
+}  
+void LatticeBoltzmann::Collision(void){
+  int ix,iy,i,n0; double rho0,Ux0,Uy0;
+  for(ix=0;ix<Lx;ix++) //for each cell
+    for(iy=0;iy<Ly;iy++){
+      //compute the macroscopic fields on the cell
+      rho0=rho(ix,iy,false); Ux0=Jx(ix,iy,false)/rho0; Uy0=Jy(ix,iy,false)/rho0;
+      for(i=0;i<Q;i++){ //for each velocity vector
+	n0=n(ix,iy,i);
+	fnew[n0]=UmUtau*f[n0]+Utau*feq(rho0,Ux0,Uy0,i);
+      }
+    }  
+}
+
+void LatticeBoltzmann::Advection(void){
+  int ix,iy,i,ixnext,iynext,n0,n0next;
+  for(ix=0;ix<Lx;ix++) //for each cell
+    for(iy=0;iy<Ly;iy++)
+      for(i=0;i<Q;i++){ //on each direction
+	ixnext=(ix+Vx[i]+Lx)%Lx; iynext=(iy+Vy[i]+Ly)%Ly;
+	n0=n(ix,iy,i); n0next=n(ixnext,iynext,i);
+	f[n0next]=fnew[n0]; //periodic boundaries
+      }
+}
+void LatticeBoltzmann::Print(const char * NameFile,double Ufan){
+  ofstream MyFile(NameFile); double rho0,Ux0,Uy0; int ix,iy;
+  for(ix=0;ix<Lx;ix+=1){
+    for(iy=0;iy<Ly;iy+=1){
+      rho0=rho(ix,iy,true); Ux0=Jx(ix,iy,true)/rho0; Uy0=Jy(ix,iy,true)/rho0;
+      MyFile<<ix<<" "<<iy<<" "<<rho0<<endl;
+    }
+    MyFile<<endl;
+  }
+  MyFile.close();
+}
+/*double LatticeBoltzmann::Varianza(void)
+            {   int ix,iy;
+                double xprom=0;
+                double N=0;
+                for(ix=0;ix<Lx;ix++)
+                    {for(iy=0;iy<Ly;iy++)
+                        {N+=rho(ix,iy,true);}
+                    }
+                for(ix=0;ix<Lx;ix++)
+                    {for(iy=0;iy<Ly;iy++)
+                        {xprom+=rho(ix,iy,true)*ix;}
+                    }
+                xprom=xprom/N;
+
+                double sum;
+                for(ix=0;ix<Lx;ix++)
+                    {for(iy=0;iy<Ly;iy++)
+                        {
+                            sum+=rho(ix,iy,true)*(ix-xprom)*(ix-xprom);
+                        }
+                    }
+		   double Varianza=sum2/N;
+                return Varianza;
+		}*/
+
+double LatticeBoltzmann::Varianza(void){
+  double N=0;
+  for(int ix=0;ix<Lx;ix++){
+    for(int iy=0;iy<Ly;iy++){
+      N+=rho(ix,iy,true);
+    }
+  }
+  double sum=0;
+  for(int ix=0;ix<Lx;ix++){
+    for(int iy=0;iy<Ly;iy++){
+      sum+=rho(ix,iy,true)*ix;
+    }
+  }
+  double xprom=sum/N;
+  double sum1=0;
+  for(int ix=0;ix<Lx;ix++){
+    for(int iy=0;iy<Ly;iy++){
+      sum1+=rho(ix,iy,true)*pow(ix-xprom,2);
+    }
+  }
+  double Varianza=sum1/N;
+  return Varianza;
+}
+
+//------------------- Funciones Globales ------------
+
+int main(int argc, char** argv){
+  LatticeBoltzmann Aire;
+  int t,tmax=atoi(argv[1]);//=10000;
+  double rho0=0,Ufan0=0;
+  // ofstream Varianza("Varianza.dat");
+  //Start
+  Aire.Start(rho0,0,0,sigma,mu);
+  //Run
+  for(t=0;t<tmax;t++){
+    Aire.Collision();
+    // Varianza<<t<<"\t"<<Aire.Varianza()<<endl;
+    Aire.Advection();
+  }
+  //Print
+  Aire.Print("Densidad.dat",Ufan0);
+  //Varianza.close();
+  return 0;
+}  
